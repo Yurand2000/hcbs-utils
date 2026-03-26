3b1986a4472abbf6d8c54808a6d903fd88f700f2
Yuri Andriaccio <yurand2000@gmail.com>
2026-03-26 17:23:08 +0100

    Refactor HCBS functions, introduce HCBSCgroup struct

--- src/cgroup/hcbs.rs
+++ src/cgroup/hcbs.rs
@@ -1,146 +1,93 @@
 use crate::prelude::*;
-use crate::utils::*;
+
+mod uni;
+mod multi;
 
 pub mod prelude {
+    pub use super::uni::prelude::*;
+    pub use super::multi::prelude::*;
     pub use super::{
-        set_cgroup_period_us,
-        set_cgroup_runtime_us,
-        get_cgroup_period_us,
-        get_cgroup_runtime_us,
-
-        set_cgroup_period_us_multi,
-        set_cgroup_runtime_us_multi,
-        get_cgroup_period_us_multi,
-        get_cgroup_runtime_us_multi,
+        HCBSCgroup,
     };
 }
 
-/// \[HCBS specific\] Set the cgroup server's period
-pub fn set_cgroup_period_us(name: &str, period_us: u64) -> anyhow::Result<()> {
-    let path = cgroup_abs_path(name);
-
-    __write_file(
-        format!("{path}/cpu.rt_period_us"),
-        format!("{period_us}")
-    )?;
-
-    info!("Set period {period_us} us to {path}/cpu.rt_period_us");
-
-    Ok(())
+pub struct HCBSCgroup {
+    name: String,
+    force_kill: bool,
 }
 
-/// \[HCBS specific\] Set the cgroup server's runtime
-pub fn set_cgroup_runtime_us(name: &str, runtime_us: u64) -> anyhow::Result<()> {
-    let path = cgroup_abs_path(name);
-
-    __write_file(
-        format!("{path}/cpu.rt_runtime_us"),
-        format!("{runtime_us}")
-    )?;
-
-    info!("Set runtime {runtime_us} us to {path}/cpu.rt_runtime_us");
-
-    Ok(())
-}
-
-/// \[HCBS specific\] Get the cgroup server's period
-pub fn get_cgroup_period_us(name: &str) -> anyhow::Result<u64> {
-    __read_file_parse(
-        format!("{}/cpu.rt_period_us", cgroup_abs_path(name)),
-        |s| s.trim().parse::<u64>()
-    )
-}
-
-/// \[HCBS specific\] Get the cgroup server's runtime
-pub fn get_cgroup_runtime_us(name: &str) -> anyhow::Result<u64> {
-    __read_file_parse(
-        format!("{}/cpu.rt_runtime_us", cgroup_abs_path(name)),
-        |s| s.trim().parse::<u64>()
-    )
-}
-
-// \[HCBS multicpu specific\] --------------------------------------------------
-pub fn get_cgroup_runtime_us_multi(name: &str) -> anyhow::Result<Vec<(u64, Vec<CpuID>)>> {
-    __read_file_parse(
-        format!("{}/cpu.rt_runtime_us", cgroup_abs_path(name)),
-        |str| parse_times(str).map(|times| pack_times_by_cpu_id(&times))
-    )
-}
-
-pub fn get_cgroup_period_us_multi(name: &str) -> anyhow::Result<Vec<(u64, Vec<CpuID>)>> {
-    __read_file_parse(
-        format!("{}/cpu.rt_period_us", cgroup_abs_path(name)),
-        |str| parse_times(str).map(|times| pack_times_by_cpu_id(&times))
-    )
-}
-
-pub fn set_cgroup_runtime_us_multi(name: &str, runtimes_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
-    let path = cgroup_abs_path(name);
-    let data = packed_to_string(runtimes_us);
-
-    __write_file(
-        format!("{path}/cpu.rt_runtime_us"),
-        &data
-    )?;
-
-    info!("Set runtimes {data} to {path}/cpu.rt_period_us");
-
-    Ok(())
-}
-
-pub fn set_cgroup_period_us_multi(name: &str, periods_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
-    let path = cgroup_abs_path(name);
-    let data = packed_to_string(periods_us);
-
-    __write_file(
-        format!("{path}/cpu.rt_period_us"),
-        &data
-    )?;
-
-    info!("Set periods {data} to {path}/cpu.rt_period_us");
-
-    Ok(())
-}
-
-fn pack_times_by_cpu_id(times_us: &[u64]) -> Vec<(u64, Vec<CpuID>)> {
-    use std::collections::HashMap;
-
-    let mut times: Vec<(u64, CpuID)> =
-        times_us.into_iter().enumerate()
-            .map(|(cpu, time_us)| (*time_us, cpu as CpuID)).collect();
-    times.sort_unstable_by_key(|(time_us, _)| *time_us);
-
-    times.into_iter().fold(HashMap::<u64, Vec<CpuID>>::new(), |mut acc, (time_us, cpu)| {
-        if time_us == 0 { return acc; }
-
-        if let Some(cpus) = acc.get_mut(&time_us) {
-            cpus.push(cpu);
-        } else {
-            acc.insert(time_us, vec![cpu]);
+impl HCBSCgroup {
+    pub fn new(name: &str) -> anyhow::Result<Self> {
+        if name == "." {
+            anyhow::bail!("Cannot handle root cgroup");
         }
 
-        acc
-    }).into_iter().collect()
-}
+        create_cgroup(name)?;
 
-fn packed_to_string(times_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> String {
-    let mut times_string =
-        times_us.map(|(time, cpus)| {
-            let mut time_str = format!("{time} ");
-            for cpu in cpus {
-                time_str += &format!("{cpu},");
+        Ok(Self {
+            name: name.to_owned(),
+            force_kill: false,
+        })
+    }
+
+    pub fn with_force_kill(mut self, force_kill: bool) -> Self {
+        self.force_kill = force_kill;
+        self
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn destroy(mut self) -> anyhow::Result<()> {
+        self.__destroy()
+    }
+
+    pub fn set_runtime_us(&mut self, runtime_us: u64) -> anyhow::Result<()> {
+        set_cgroup_runtime_us(&self.name, runtime_us)
+    }
+
+    pub fn set_period_us(&mut self, period_us: u64) -> anyhow::Result<()> {
+        set_cgroup_period_us(&self.name, period_us)
+    }
+
+    pub fn set_runtime_us_multi(&mut self, runtimes_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
+        set_cgroup_runtime_us_multi(&self.name, runtimes_us)
+    }
+
+    pub fn set_period_us_multi(&mut self, periods_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
+        set_cgroup_period_us_multi(&self.name, periods_us)
+    }
+
+    pub fn set_runtime_us_multi_str(&mut self, runtimes_us: &str) -> anyhow::Result<()> {
+        set_cgroup_runtime_us_multi_str(&self.name, runtimes_us)
+    }
+
+    pub fn set_period_us_multi_str(&mut self, periods_us: &str) -> anyhow::Result<()> {
+        set_cgroup_period_us_multi_str(&self.name, periods_us)
+    }
+
+    fn __destroy(&mut self) -> anyhow::Result<()> {
+        if !cgroup_exists(&self.name) { return Ok(()); }
+
+        if self.force_kill {
+            if is_pid_in_cgroup(&self.name, std::process::id())? {
+                assign_pid_to_cgroup(".", std::process::id())?;
             }
 
-            time_str.pop();
-            time_str
-        }).fold(String::new(), |acc, time_str| acc + &time_str + " ");
+            cgroup_pids(&self.name)?.iter()
+                .try_for_each(|pid| {
+                    kill_pid(*pid)?;
+                    assign_pid_to_cgroup(".", *pid)
+                })?;
+        }
 
-    times_string.pop();
-    times_string
+        delete_cgroup(&self.name)
+    }
 }
 
-fn parse_times(times: String) -> anyhow::Result<Vec<u64>> {
-    times.trim().split_ascii_whitespace()
-        .map(|s| s.parse::<u64>().map_err(|err| err.into()))
-        .collect()
+impl Drop for HCBSCgroup {
+    fn drop(&mut self) {
+        let _ = self.__destroy();
+    }
 }
---
+++ src/cgroup/hcbs/multi.rs
@@ -0,0 +1,144 @@
+use crate::prelude::*;
+use crate::utils::*;
+
+pub mod prelude {
+    pub use super::{
+        set_cgroup_period_us_multi,
+        set_cgroup_runtime_us_multi,
+        get_cgroup_period_us_multi,
+        get_cgroup_runtime_us_multi,
+        set_cgroup_period_us_multi_str,
+        set_cgroup_runtime_us_multi_str,
+    };
+}
+
+// \[HCBS multicpu specific\] --------------------------------------------------
+fn parse_times_on_get(times: String) -> anyhow::Result<Vec<(u64, Vec<CpuID>)>> {
+    use std::collections::HashMap;
+
+    let times: anyhow::Result<Vec<_>> =
+        times.trim().split_ascii_whitespace()
+        .map(|s| s.parse::<u64>().map_err(|err| err.into()))
+        .collect();
+
+    let mut times: Vec<(u64, CpuID)> =
+        times?.into_iter().enumerate()
+            .map(|(cpu, time_us)| (time_us, cpu as CpuID)).collect();
+    times.sort_unstable_by_key(|(time_us, _)| *time_us);
+
+    Ok(times.into_iter().fold(HashMap::<u64, Vec<CpuID>>::new(), |mut acc, (time_us, cpu)| {
+        if time_us == 0 { return acc; }
+
+        if let Some(cpus) = acc.get_mut(&time_us) {
+            cpus.push(cpu);
+        } else {
+            acc.insert(time_us, vec![cpu]);
+        }
+
+        acc
+    }).into_iter().collect())
+}
+
+/// \[HCBS MultiCPU Specific\] Get the cgroup server's runtime
+pub fn get_cgroup_runtime_us_multi(name: &str) -> anyhow::Result<Vec<(u64, Vec<CpuID>)>> {
+    __read_file_parse(
+        format!("{}/cpu.rt_runtime_us", cgroup_abs_path(name)),
+        |str| parse_times_on_get(str)
+    )
+}
+
+/// \[HCBS MultiCPU Specific\] Get the cgroup server's period
+pub fn get_cgroup_period_us_multi(name: &str) -> anyhow::Result<Vec<(u64, Vec<CpuID>)>> {
+    __read_file_parse(
+        format!("{}/cpu.rt_period_us", cgroup_abs_path(name)),
+        |str| parse_times_on_get(str)
+    )
+}
+
+/// \[HCBS MultiCPU Specific\] Set the cgroup server's runtime
+pub fn set_cgroup_runtime_us_multi(name: &str, runtimes_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
+    let path = cgroup_abs_path(name);
+    let data = packed_to_string(runtimes_us);
+
+    __write_file(
+        format!("{path}/cpu.rt_runtime_us"),
+        &data
+    )?;
+
+    info!("Set runtimes {data} to {path}/cpu.rt_runtime_us");
+
+    Ok(())
+}
+
+/// \[HCBS MultiCPU Specific\] Set the cgroup server's period
+pub fn set_cgroup_period_us_multi(name: &str, periods_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> anyhow::Result<()> {
+    let path = cgroup_abs_path(name);
+    let data = packed_to_string(periods_us);
+
+    __write_file(
+        format!("{path}/cpu.rt_period_us"),
+        &data
+    )?;
+
+    info!("Set periods {data} to {path}/cpu.rt_period_us");
+
+    Ok(())
+}
+
+fn packed_to_string(times_us: impl Iterator<Item = (u64, impl Iterator<Item = u32>)>) -> String {
+    let mut times_string =
+        times_us.map(|(time, cpus)| {
+            let mut time_str = format!("{time} ");
+            for cpu in cpus {
+                time_str += &format!("{cpu},");
+            }
+
+            time_str.pop();
+            time_str
+        }).fold(String::new(), |acc, time_str| acc + &time_str + " ");
+
+    times_string.pop();
+    times_string
+}
+
+// HCBS Specific setters from str ----------------------------------------------
+fn parse_times_on_set(times_us: &str) -> anyhow::Result<Vec<(u64, CpuSetUnchecked)>> {
+    use std::str::FromStr as _;
+    use nom::Parser;
+    use nom::multi::*;
+    use nom::character::complete::*;
+    use nom::combinator::*;
+
+    let uint = || {
+        digit1::<&str, ()>
+            .map_res(|num: &str| num.parse::<u64>())
+    };
+
+    let cpu_set = || {
+        recognize(many1(one_of("0123456789-,")))
+            .map_res(|str: &str| CpuSetUnchecked::from_str(str))
+    };
+
+    let time_set = || {
+        map((uint(), space1, cpu_set()), |(time, _, set)| (time, set))
+    };
+
+    separated_list1(space1, time_set()).parse(times_us)
+        .map(|(_, data)| data)
+        .map_err(|err| {
+            log::error!("Parse error for time string: {}", times_us);
+            err.into()
+        })
+}
+
+/// \[HCBS MultiCPU Specific\] Set the cgroup server's runtime (from str)
+pub fn set_cgroup_runtime_us_multi_str(name: &str, runtimes_us: &str) -> anyhow::Result<()> {
+    set_cgroup_runtime_us_multi(name, parse_times_on_set(runtimes_us)?
+        .into_iter().map(|(time, cpus)| (time, cpus.into_iter())) )
+}
+
+/// \[HCBS MultiCPU Specific\] Set the cgroup server's period (from str)
+pub fn set_cgroup_period_us_multi_str(name: &str, periods_us: &str) -> anyhow::Result<()> {
+    set_cgroup_period_us_multi(name, parse_times_on_set(periods_us)?
+        .into_iter().map(|(time, cpus)| (time, cpus.into_iter())) )
+}
